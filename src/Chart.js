1bdab1e17b52c86b8a26cdc1f0efb87f0ad9debf
jmalvarez97 <jualvarez@fi.uba.ar>
2023-08-17 21:40:48 -0300

    cambios esteticos

--- src/Chart.js
+++ src/Chart.js
@@ -3,17 +3,22 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from "r
 import "./Chart.css";
 
 export function Chart({ data, secadorIndex }) {
+  const formatXAxis = (tickItem) => {
+    const timeParts = tickItem.split(" ")[1].split(":");
+    return `${timeParts[0]}:${timeParts[1]}`;
+  };
+
   return (
     <div className="line-chart">
       <h3> R% - Secadero {secadorIndex + 1}</h3>
       <div className="chart-container">
-        <LineChart width={400} height={300} data={data}>
+        <LineChart width={330} height={250} data={data}>
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" />
+          <XAxis dataKey="date" tickFormatter={formatXAxis} />
           <YAxis domain={[40, "dataMax + 10"]} /> 
           <Tooltip />
           <Legend />
-          <Line type="monotone" dataKey={`s${secadorIndex + 1}h`} stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey={`s${secadorIndex + 1}h`} stroke="#8884d8" activeDot={false} />
         </LineChart>
       </div>
     </div>
