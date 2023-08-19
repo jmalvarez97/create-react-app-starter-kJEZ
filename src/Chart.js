58db7da76d95820c11be3709d3bda29560bc594b
jmalvarez97 <jualvarez@fi.uba.ar>
2023-08-19 13:03:32 -0300

    muestra h y t

--- src/Chart.js
+++ src/Chart.js
@@ -8,17 +8,27 @@ export function Chart({ data, secadorIndex }) {
     return `${timeParts[0]}:${timeParts[1]}`;
   };
 
+  // Calcula los valores mínimos y máximos de humedad y temperatura en los datos
+  const humidityValues = data.map((entry) => entry[`s${secadorIndex + 1}h`]);
+  const temperatureValues = data.map((entry) => entry[`s${secadorIndex + 1}t`]);
+  const minHumidity = Math.min(...humidityValues);
+  const maxHumidity = Math.max(...humidityValues);
+  const minTemperature = Math.min(...temperatureValues);
+  const maxTemperature = Math.max(...temperatureValues);
+
   return (
     <div className="line-chart">
-      <h3> R% - Secadero {secadorIndex + 1}</h3>
+      <h3> R% y Temperatura - Secadero {secadorIndex + 1}</h3>
       <div className="chart-container">
         <LineChart width={330} height={250} data={data}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="date" tickFormatter={formatXAxis} />
-          <YAxis domain={["dataMin - 10", "dataMax + 10"]} /> 
+          <YAxis yAxisId="humidity" domain={[minHumidity - 5, maxHumidity + 5]} label={{ value: "Humedad (%)", angle: -90, position: 'insideLeft' }} />
+          <YAxis yAxisId="temperature" orientation="right" domain={[minTemperature - 2, maxTemperature + 2]} label={{ value: "Temperatura (°C)", angle: 90, position: 'insideRight' }} />
           <Tooltip />
           <Legend />
-          <Line type="monotone" dataKey={`s${secadorIndex + 1}h`} stroke="#8884d8" activeDot={false} />
+          <Line yAxisId="humidity" type="monotone" dataKey={`s${secadorIndex + 1}h`} stroke="#8884d8" name="Humedad" activeDot={false} />
+          <Line yAxisId="temperature" type="monotone" dataKey={`s${secadorIndex + 1}t`} stroke="#82ca9d" name="Temperatura" activeDot={false} />
         </LineChart>
       </div>
     </div>
